28b08bf0b1ba4b3f8fac22c5dd6c4a8f9bd3f874
871181099@qq.com <871181099@qq.com>
2021-07-11 11:12:16 +0800

    pramscustom

--- src/ui/views/paramsManage/paramscustom/index.vue
+++ src/ui/views/paramsManage/paramscustom/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-06-23 14:30:28
- * @LastEditTime: 2021-07-02 10:44:08
+ * @LastEditTime: 2021-07-11 11:07:21
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \properties-web\src\ui\views\paramsManage\paramscustom\index.vue
